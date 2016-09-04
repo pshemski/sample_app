deab25c2659d71e977b0696db1b161a63dfadc14
Przemek Markowski <p.markowski1980@gmail.com>
2016-09-04 23:55:50 +0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-	root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
+  root 'static_pages#home'
+  get '/help' => 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
 end
