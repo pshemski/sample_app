96b0a40c5e1684b3c4cfbf36ee7b642b8d821e4f
Przemek Markowski <p.markowski1980@gmail.com>
2016-09-07 00:12:38 +0700

    Implemented basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+	# ( '=>' and ', to:' ) do the same
   root 'static_pages#home'
   get '/help' => 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
 end
